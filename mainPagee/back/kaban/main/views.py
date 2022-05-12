70f80f101c93561ffab6656fdc2d032bb4f00f08
LAPTOP-DJQ9H9R6\user <ostrovatikoval@mail.ru>
2022-05-13 00:15:23 +0700

    no message

--- mainPagee/back/kaban/main/views.py
+++ mainPagee/back/kaban/main/views.py
@@ -33,11 +33,8 @@ def register(request):
     if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
-            # Create a new user object but avoid saving it yet
             new_user = user_form.save(commit=False)
-            # Set the chosen password
             new_user.set_password(user_form.cleaned_data['password1'])
-            # Save the User object
             new_user.save()
             #return render(request, 'main/main.html', {'new_user': new_user})
             return redirect('home')
